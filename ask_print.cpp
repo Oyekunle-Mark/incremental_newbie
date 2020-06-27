deaca67eb02110f77e44e51681442a52a62e2adf
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-06-27 20:10:38 +0100

    The ask_print program

--- ask_print.cpp
+++ ask_print.cpp
@@ -6,7 +6,6 @@ int main()
 
     std::cout << "Enter your favourite number between 1 and 100: ";
     std::cin >> favourite_number;
-
     std::cout << "Sweet!! That's my favourite number too!" << std::endl;
 
     return 0;
