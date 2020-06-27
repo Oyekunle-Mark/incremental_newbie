a4c2820169a15171d2b367f78dbacd2b7c797e3f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-06-27 20:52:18 +0100

    Added the section quiz

--- structure/main.cpp
+++ structure/main.cpp
@@ -7,6 +7,7 @@ int main()
     std::cout << "Enter your favourite number between 1 and 100: ";
     std::cin >> favourite_number;
     std::cout << "Sweet!! That's my favourite number too!" << std::endl;
+    std::cout << "No really, " << favourite_number << " is my favourite number!" << std::endl;
 
     return 0;
 }
