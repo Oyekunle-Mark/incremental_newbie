ccce51c983de1087cdc923c6bdaf8bdff4c20db5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-07-03 18:39:53 +0100

    Modified display

--- section_7/main.cpp
+++ section_7/main.cpp
@@ -33,18 +33,14 @@ int main()
     vector2d.push_back(vector2);
 
     cout << "\nThe elements of vector2d are: " << endl;
-    cout << vector2d.at(0).at(0) << endl;
-    cout << vector2d.at(0).at(1) << endl;
-    cout << vector2d.at(1).at(0) << endl;
-    cout << vector2d.at(1).at(1) << endl;
+    cout << vector2d.at(0).at(0) << "\t" << vector2d.at(0).at(1) << endl;
+    cout << vector2d.at(1).at(0) << "\t" << vector2d.at(1).at(1) << endl;
 
     vector1.at(0) = 1000;
 
     cout << "\nThe elements of vector2d are: " << endl;
-    cout << vector2d.at(0).at(0) << endl;
-    cout << vector2d.at(0).at(1) << endl;
-    cout << vector2d.at(1).at(0) << endl;
-    cout << vector2d.at(1).at(1) << endl;
+    cout << vector2d.at(0).at(0) << "\t" << vector2d.at(0).at(1) << endl;
+    cout << vector2d.at(1).at(0) << "\t" << vector2d.at(1).at(1) << endl;
 
     cout << "\nThe elements in vector1 are: " << endl;
     cout << vector1.at(0) << endl;
