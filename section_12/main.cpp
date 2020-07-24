af73cd89957f40623839855f93f30ab9643915e9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-07-24 09:16:37 +0100

    Mocked program flow

--- section_12/main.cpp
+++ section_12/main.cpp
@@ -7,6 +7,21 @@ int *apply_all(const int *const arr1, size_t size1, const int *const arr2, size_
 
 int main()
 {
+    int arr1[] {1, 2, 3, 4, 5};
+    int arr2[] {1, 2, 3};
+
+    cout << "Array 1: ";
+    print_array(arr1, 5);
+
+    cout << "Array 2: ";
+    print_array(arr2, 5);
+
+    int *result = apply_all(arr1, 5, arr2, 3);
+
+    cout << "Result: ";
+    print_array(result, 15);
+
+    delete [] result;
 }
 
 void print_array(const int *const arr, size_t size)
