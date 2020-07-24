8f690ee9a1065c3d40e2978e0669bb8c8f60747d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-07-24 09:32:46 +0100

    Completed the apply_all function

--- section_12/main.cpp
+++ section_12/main.cpp
@@ -7,8 +7,8 @@ int *apply_all(const int *const arr1, size_t size1, const int *const arr2, size_
 
 int main()
 {
-    int arr1[] {1, 2, 3, 4, 5};
-    int arr2[] {1, 2, 3};
+    int arr1[]{1, 2, 3, 4, 5};
+    int arr2[]{10, 20, 30};
 
     cout << "Array 1: ";
     print_array(arr1, 5);
@@ -21,7 +21,7 @@ int main()
     cout << "Result: ";
     print_array(result, 15);
 
-    delete [] result;
+    delete[] result;
 }
 
 void print_array(const int *const arr, size_t size)
@@ -33,4 +33,16 @@ void print_array(const int *const arr, size_t size)
 
 int *apply_all(const int *const arr1, size_t size1, const int *const arr2, size_t size2)
 {
+    int *result{nullptr};
+    result = new int[size1 * size2];
+    int pos {0};
+
+    for (size_t i{}; i < size2; ++i) {
+        for (size_t j{}; j < size1; ++j) {
+            result[pos] = arr2[i] * arr1[j];
+            ++pos;
+        }
+    }
+
+    return result;
 }
