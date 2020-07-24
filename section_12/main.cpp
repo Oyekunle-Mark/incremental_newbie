f291fc51c1dc2ea78e7d42c8b65daaeed00aec3e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-07-24 09:09:05 +0100

    Solution template

---
+++ section_12/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void print_array(const int *const arr, size_t size);
+int *apply_all(const int *const arr1, size_t size1, const int *const arr2, size_t size2);
+
+int main()
+{
+}
+
+void print_array(const int *const arr, size_t size)
+{
+}
+
+int *apply_all(const int *const arr1, size_t size1, const int *const arr2, size_t size2)
+{
+}
