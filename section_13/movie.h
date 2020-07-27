fce993cc63a6a21f8cc34c0beb6f539ca3eca6c3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-07-27 20:52:25 +0100

    Members, getters and setters

---
+++ section_13/movie.h
@@ -0,0 +1,28 @@
+#ifndef _MOVIE_H_
+#define _MOVIE_H_
+#include <iostream>
+#include <string>
+
+class Movie
+{
+private:
+    std::string movie_name;
+    std::string movie_rating;
+    size_t watched;
+
+public:
+    void set_name(std::string name) { movie_name = name; }
+    std::string get_name() const { return movie_name; }
+    void set_movie_rating(std::string rating) { movie_rating = rating; }
+    std::string get_movie_rating() const { return movie_rating; }
+    void increment_watched() { watched++; }
+    size_t get_watched() const { return watched; }
+    void display_details() const
+    {
+        std::cout << movie_name << " "
+                  << movie_rating << " "
+                  << watched << std::endl;
+    }
+};
+
+#endif;
