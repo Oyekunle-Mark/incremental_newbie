f013b7c6745513c2a2042a349b164d850fd9dd5d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-01 18:48:19 +0100

    Utils header files

---
+++ section_15/Account_Utils.h
@@ -0,0 +1,15 @@
+#ifndef _ACCOUNT_UTILS_H_
+#define _ACCOUNT_UTILS_H_
+#include <vector>
+#include "Account.h"
+#include "Savings_Account.h"
+
+void display_accounts(const std::vector<Account> &);
+void deposit_all_accounts(const std::vector<Account> &, double);
+void withdraw_all_accounts(const std::vector<Account> &, double);
+
+void display_savings_accounts(const std::vector<Savings_Account> &);
+void deposit_all_savings_accounts(const std::vector<Savings_Account> &, double);
+void withdraw_all_savings_accounts(const std::vector<Savings_Account> &, double);
+
+#endif
