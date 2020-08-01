33cd6c4f59db586a768e87003e6bd0270ad8252b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-01 19:26:07 +0100

    checking account header file

---
+++ section_15/Checking_Account.h
@@ -0,0 +1,16 @@
+#ifndef _CHECKING_ACCOUNT_H_
+#define _CHECKING_ACCOUNT_H_
+#include "Account.h"
+
+class Checking_Account : public Account
+{
+private:
+    const double withdrawal_fee{1.5};
+    friend std::ostream &operator<<(const std::ostream &output, const Checking_Account &rhs);
+
+public:
+    Checking_Account(std::string name, double balance);
+    void withdraw(double amount);
+}
+
+#endif
