8a41809e316b364223e6cd910fde698f123582f3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-01 18:07:09 +0100

    Account header file

---
+++ section_15/Account.h
@@ -0,0 +1,22 @@
+#ifndef _ACCOUNT_H_
+#define _ACCOUNT_H_
+#include <string>
+#include <iostream>
+
+class Account
+{
+private:
+    std::string name;
+    double amount;
+    friend std::ostream &operator<<(std::ostream &os, const Account &rhs);
+
+protected:
+    double get_balance();
+
+public:
+    Account(std::string name, double amount = 0.0);
+    void withdraw(double amount);
+    void deposit(double amount);
+};
+
+#endif
