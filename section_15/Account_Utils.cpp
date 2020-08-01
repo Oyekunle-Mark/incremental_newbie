59ca5256f06ec23f121025b5848e32015fa82782
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-01 19:16:46 +0100

    Implemented the savings account

--- section_15/Account_Utils.cpp
+++ section_15/Account_Utils.cpp
@@ -26,3 +26,30 @@ void withdraw_all_accounts(std::vector<Account> &accounts, double amount)
     for (Account &account : accounts)
         account.withdraw(amount);
 }
+
+void display_savings_accounts(const std::vector<Savings_Account> &accounts)
+{
+    std::cout << "\nDisplaying all savings accounts\n"
+              << std::endl;
+
+    for (const Savings_Account &account : accounts)
+        std::cout << account;
+}
+
+void deposit_all_savings_accounts(std::vector<Savings_Account> &accounts, double amount)
+{
+    std::cout << "\nDepositing " << amount << " to all savings accounts\n"
+              << std::endl;
+
+    for (Savings_Account &account : accounts)
+        account.deposit(amount);
+}
+
+void withdraw_all_savings_accounts(std::vector<Savings_Account> &accounts, double amount)
+{
+    std::cout << "\nWithdrawing " << amount << " from all savings accounts\n"
+              << std::endl;
+
+    for (Savings_Account &account : accounts)
+        account.withdraw(amount);
+}
--- section_15/main.cpp
+++ section_15/main.cpp
@@ -2,13 +2,27 @@
 
 int main()
 {
+    // ordinary account type
     std::vector<Account> account_vec;
-    account_vec.push_back(Account{"User 1"});
-    account_vec.push_back(Account{"User 2", 100.0});
-    account_vec.push_back(Account{"User 2", 353434.3});
+    account_vec.push_back(Account{"Account 1"});
+    account_vec.push_back(Account{"Account 2", 100.0});
+    account_vec.push_back(Account{"Account 2", 353434.3});
 
     display_accounts(account_vec);
     deposit_all_accounts(account_vec, 2000);
+    display_accounts(account_vec);
     withdraw_all_accounts(account_vec, 3000);
     display_accounts(account_vec);
+
+    // savings account type
+    std::vector<Savings_Account> savings_account_vec;
+    savings_account_vec.push_back(Savings_Account{"Savings Account 1"});
+    savings_account_vec.push_back(Savings_Account{"Savings Account 2", 500});
+    savings_account_vec.push_back(Savings_Account{"Savings Account 3", 2324, 5});
+
+    display_savings_accounts(savings_account_vec);
+    deposit_all_savings_accounts(savings_account_vec, 500);
+    display_savings_accounts(savings_account_vec);
+    withdraw_all_savings_accounts(savings_account_vec, 600);
+    display_savings_accounts(savings_account_vec);
 }
