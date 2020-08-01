57a5dc6aebaf512b3eb93633c23e4132068790e6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-01 19:34:47 +0100

    Checking account implementation

--- section_15/Account_Utils.h
+++ section_15/Account_Utils.h
@@ -2,6 +2,7 @@
 #define _ACCOUNT_UTILS_H_
 #include <vector>
 #include "Savings_Account.h"
+#include "Checking_Account.h"
 
 void display_accounts(const std::vector<Account> &);
 void deposit_all_accounts(std::vector<Account> &, double);
@@ -10,5 +11,9 @@ void withdraw_all_accounts(std::vector<Account> &, double);
 void display_savings_accounts(const std::vector<Savings_Account> &);
 void deposit_all_savings_accounts(std::vector<Savings_Account> &, double);
 void withdraw_all_savings_accounts(std::vector<Savings_Account> &, double);
+
+void display_checking_accounts(const std::vector<Checking_Account> &);
+void deposit_all_checking_accounts(std::vector<Checking_Account> &, double);
+void withdraw_all_checking_accounts(std::vector<Checking_Account> &, double);
 
 #endif
