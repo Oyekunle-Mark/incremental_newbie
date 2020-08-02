f074d1d289d875fc632206c84e766b959062d0a0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-02 12:03:35 +0100

    Extracted I  printable from accoont

--- section_15_and_16/Account.h
+++ section_15_and_16/Account.h
@@ -2,21 +2,7 @@
 #define _ACCOUNT_H_
 #include <string>
 #include <iostream>
-
-class I_Printable
-{
-    friend std::ostream &operator<<(std::ostream &output, const I_Printable &rhs);
-
-public:
-    virtual void print(std::ostream &output) const = 0;
-    ~I_Printable() = default;
-};
-
-std::ostream &operator<<(std::ostream &output, const I_Printable &rhs)
-{
-    rhs.print(output);
-    return output;
-}
+#include "I_Printable.h"
 
 class Account : public I_Printable
 {
