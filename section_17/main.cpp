0aa5f838998699f339f3ebd99ce16a1d11dcfdac
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-02 20:19:45 +0100

    Declared class type

---
+++ section_17/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Test;
+
+int main()
+{
+}
+
+class Test
+{
+private:
+    int data;
+
+public:
+    Test(int data) : data{data}
+    {
+        std::cout << "\tTest Constructor (" << data << ")" << std::endl;
+    }
+    Test() : Test(0) {}
+    int get_data() const
+    {
+        return data;
+    }
+    ~Test()
+    {
+        std::cout << "\tTest Destructor (" << data << ")" << std::endl;
+    }
+};
