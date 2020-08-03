71c69685db47d95894f30af7b14e8ab2a33829d1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-03 13:00:08 +0100

    Tested the illegal exception class

--- section_15_and_16_and_18/main.cpp
+++ section_15_and_16_and_18/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <memory>
+#include "IllegalBalanceException.h"
 #include "Account_Utils.h"
 
 int main()
@@ -8,7 +10,7 @@ int main()
     Account *sav1 = new Savings_Account{"Savings Account 1"};
     Account *sav2 = new Savings_Account{"Savings Account 2", 500};
     Account *sav3 = new Savings_Account{"Savings Account 3", 2324, 5};
-    std::vector<Account *> savings_account_vec {sav1,sav2,sav3};
+    std::vector<Account *> savings_account_vec{sav1, sav2, sav3};
 
     display_accounts(savings_account_vec);
     deposit_all_accounts(savings_account_vec, 500);
@@ -20,7 +22,7 @@ int main()
     Account *check1 = new Checking_Account{"Checking Account 1"};
     Account *check2 = new Checking_Account{"Checking Account 2", 941};
     Account *check3 = new Checking_Account{"Checking Account 3", 12};
-    std::vector<Account *> checking_account_vec {check1,check2,check3};
+    std::vector<Account *> checking_account_vec{check1, check2, check3};
 
     display_accounts(checking_account_vec);
     deposit_all_accounts(checking_account_vec, 320);
@@ -32,7 +34,7 @@ int main()
     Account *trust1 = new Trust_Account{"Trust Account 1"};
     Account *trust2 = new Trust_Account{"Trust Account 2", 1500, 7};
     Account *trust3 = new Trust_Account{"Trust Account 3", 43000, 10};
-    std::vector<Account *> trust_account_vec {trust1,trust2,trust3};
+    std::vector<Account *> trust_account_vec{trust1, trust2, trust3};
 
     display_accounts(trust_account_vec);
     deposit_all_accounts(trust_account_vec, 6000);
@@ -57,7 +59,8 @@ int main()
     // using object on the stack
     // savings account type
     Savings_Account sav{"Stack Savings Acc"};
-    std::cout << "\n === Savings Account === \n" << std::endl;
+    std::cout << "\n === Savings Account === \n"
+              << std::endl;
     std::cout << sav << std::endl;
     sav.deposit(200);
     std::cout << sav << std::endl;
@@ -66,7 +69,8 @@ int main()
 
     // checking account type
     Checking_Account check{"Stack Checking Acc", 3235};
-    std::cout << "\n === Checking Account === \n" << std::endl;
+    std::cout << "\n === Checking Account === \n"
+              << std::endl;
     std::cout << check << std::endl;
     check.deposit(200);
     std::cout << check << std::endl;
@@ -75,7 +79,8 @@ int main()
 
     // trust account type
     Trust_Account trust{"Stack Trust Acc", 3235};
-    std::cout << "\n === Trust Account === \n" << std::endl;
+    std::cout << "\n === Trust Account === \n"
+              << std::endl;
     std::cout << trust << std::endl;
     trust.deposit(200);
     std::cout << trust << std::endl;
@@ -85,6 +90,16 @@ int main()
     trust.withdraw(10);
     trust.withdraw(10);
     trust.withdraw(10);
+
+    // testing exceptions
+    try
+    {
+        std::unique_ptr<Savings_Account> sav_ptr = std::make_unique<Savings_Account>("Sample Savings Acct", -1000, 5);
+    }
+    catch (const IllegalBalanceException &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 
     return 0;
 }
