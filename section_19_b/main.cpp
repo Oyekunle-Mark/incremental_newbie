39322e91a39410aad41f6d849404bfac2e2de092
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-04 12:21:58 +0100

    First pass solution to the problem

--- section_19_b/main.cpp
+++ section_19_b/main.cpp
@@ -6,6 +6,8 @@ int main()
 {
     const std::string input_file_name{"romeo_and_juliet.txt"};
     const std::string output_file_name{"romeo_and_juliet_out.txt"};
+    std::string line{};
+    size_t line_number = 1;
 
     std::ifstream in_file{input_file_name};
     std::ofstream out_file{output_file_name};
@@ -21,6 +23,15 @@ int main()
         std::cerr << "Error creating file" << std::endl;
         return 2;
     }
+
+    while (std::getline(in_file, line))
+    {
+        out_file << line_number << "\t" << line << std::endl;
+        ++line_number;
+    }
+
+    in_file.close();
+    out_file.close();
 
     return 0;
 }
