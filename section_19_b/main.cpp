1aace4ede070e096442eb25a454af7f2edc84f7d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-04 12:19:05 +0100

    Basic file opening

--- section_19_b/main.cpp
+++ section_19_b/main.cpp
@@ -4,5 +4,23 @@
 
 int main()
 {
+    const std::string input_file_name{"romeo_and_juliet.txt"};
+    const std::string output_file_name{"romeo_and_juliet_out.txt"};
+
+    std::ifstream in_file{input_file_name};
+    std::ofstream out_file{output_file_name};
+
+    if (!in_file)
+    {
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
+    }
+
+    if (!out_file)
+    {
+        std::cerr << "Error creating file" << std::endl;
+        return 2;
+    }
+
     return 0;
 }
