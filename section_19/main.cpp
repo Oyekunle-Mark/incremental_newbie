ed363171acba4139ae62c8d023823c03ff01c3af
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-04 09:59:59 +0100

    Bare file strucrture

--- section_19/main.cpp
+++ section_19/main.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
 int main()
 {
+    std::ifstream in_file;
+    in_file.open("response.txt");
+
+    if (!in_file)
+    {
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
+    }
+
+    std::cout << "File read successfully" << std::endl;
+
+    in_file.close();
+
     return 0;
 }
