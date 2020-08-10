7ecfb14edb6aff22d29dbb54aa8cca143cb57e58
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-10 17:08:46 +0100

    Used smart pointers instead

--- cmake/use_student_lib/main.cpp
+++ cmake/use_student_lib/main.cpp
@@ -1,10 +1,10 @@
+#include <memory>
 #include "Student.h"
 
 int main(int argc, char *argv[])
 {
-    Student *student = new Student("Bidden");
-    student->display();
-    delete student;
+    std::unique_ptr<Student> s = std::make_unique<Student>("Bidden");
+    s->display();
 
     return 0;
 }
